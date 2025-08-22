65ddc8940963647de0b66b7026be1e1f240eda0d
ammaryy <ammar.yagoub3@gmail.com>
2025-08-22 20:59:18 +0300

    build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 // vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc' // or '@vitejs/plugin-react' if that's what you use
 
 export default defineConfig({
+  base: '/Portfolio/',   // <- EXACT repo name, with leading & trailing slashes
   plugins: [react()],
-  // set base only if deploying to GitHub Pages under a repo path:
-  base: '/Portfolio/',
 })
